84c5b86f5b004b32da14ac73d8c6622028d9261e
Rafee <rafeehaq2006@gmail.com>
2026-08-13 17:44:11 +0100

    another file typo found and fixed when attempting to deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 const PORT = process.env.PORT || 3000;
 const db = require("./db/query");
 const dashboardRoutes = require("./routes/dashboardRoute");
-const authRoutes = require("./routes/authRoute");
+const authRoutes = require("./routes/Authroute");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
